c0384266169c3da1b3c5c52b3ecf2b8a6cd5a20e
Damian <froispro6969@gmail.com>
2024-05-16 16:38:08 +0200

    Created titlecards

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import './TitleCards.css'
+import cards_data from '../../assets/cards/Cards_data'
+import play_icon from '../../assets/play_icon.png'
+
+
 
 const TitleCards = () => {
+  
+
+
   return (
     <div className='titlecards'>
-      
+      <h2>Popular on Netflix</h2>
+      <div className="card-list">
+        {cards_data.map((card, index) => {
+          return <div className="card" key={index}>
+            <img src={card.image} alt="" />
+            <p>{card.name}</p>
+          </div>
+        })}
+        
+      </div>
     </div>
   )
 }
