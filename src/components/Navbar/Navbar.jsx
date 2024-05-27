2334703d28dce96175b66e982f4ddefde5ff067c
Damian <froispro6969@gmail.com>
2024-05-27 10:44:16 +0200

    Created navbar searchbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
 import search_icon from '../../assets/search_icon.svg'
@@ -10,44 +10,50 @@ import { logout } from '../../pages/Login/Login'
 const Navbar = () => {
 
   const navRef = useRef();
+  const  [IsVisible, SetIsVisible] = useState(false);
 
   useEffect(() => {
     window.addEventListener('scroll', () => {
-      if(window.scrollY >= 80)
-        {
-          navRef.current.classList.add('nav-dark')
-        }
-        else {
-          navRef.current.classList.remove('nav-dark')
-        }
+      if (window.scrollY >= 80) {
+        navRef.current.classList.add('nav-dark')
+      }
+      else {
+        navRef.current.classList.remove('nav-dark')
+      }
     })
-  },[])
+  }, [])
 
   return (
     <div ref={navRef} className='navbar'>
-        <div className="navbar-left">
-          <img src={logo} alt="" />
-          <ul>
-            <li>Home</li>
-            <li>TV Shows</li>
-            <li>Movies</li>
-            <li>New & Popular</li>
-            <li>My list</li>
-            <li>Browse by Languages</li>
-          </ul>
-        </div>
-        <div className="navbar-right">
-          <img src={search_icon} alt="" className='icons'/>
-          <p>Children</p>
-          <img src={bell_icon} alt="" className='icons'/>
-          <div className="navbar-profile">
-            <img src={profile_img} alt="" className='profile'/>
-            <img src={caret_icon} alt=""/>
-            <div className="dropdown">
-              <p onClick={logout}>Sign Out of Netflix</p>
-            </div>
+      <div className="navbar-left">
+        <img src={logo} alt="" />
+        <ul>
+          <li>Home</li>
+          <li>TV Shows</li>
+          <li>Movies</li>
+          <li>New & Popular</li>
+          <li>My list</li>
+          <li>Browse by Languages</li>
+        </ul>
+      </div>
+      <div className="navbar-right">
+        {IsVisible ?
+          <div className="search-box">
+            <input type="text" placeholder='Title of Movie'/>
+            <a className='close-input' onClick={() => {SetIsVisible(false)}}>X</a>
+          </div>
+          :
+          <img src={search_icon} alt="" className='icons' onClick={() => {SetIsVisible(true)}}/>}
+        <p>Children</p>
+        <img src={bell_icon} alt="" className='icons' />
+        <div className="navbar-profile">
+          <img src={profile_img} alt="" className='profile' />
+          <img src={caret_icon} alt="" />
+          <div className="dropdown">
+            <p onClick={logout}>Sign Out of Netflix</p>
           </div>
         </div>
+      </div>
     </div>
   )
 }
